2e7aa5cb4fe8730be6a8ef42f193887f62e05290
pawel2004 <44431851+pawel2004@users.noreply.github.com>
2019-01-14 17:20:48 +0100

    Some tkinter training.
    Warning!!!
    It is not a completed project. I'm still working on it and I'm still learning tkinter. It will be probably completed when I will learn how to write events...

---
+++ Kalkulator(not completed).py
@@ -0,0 +1,172 @@
+from tkinter import *
+from tkinter import ttk
+
+class Calculator:
+
+    def __init__(self):
+        self.display = Tk()
+
+        self.display.geometry('500x500')
+
+        #Label
+        self.screen = ttk.Label(self.display, text= "0")
+        self.screen.config(justify= CENTER)
+        self.screen.config(background= "grey", foreground= "white")
+        self.screen.config(font= ("Times New Roman",20,"bold"))
+        self.screen.pack()
+
+        #Numbers
+        self.button_one = ttk.Button(self.display, text= "1")
+        self.button_one.pack()
+        self.button_one.config(command= self.add_one)
+        self.button_two = ttk.Button(self.display, text="2", command=self.add_two).pack()
+        self.button_three = ttk.Button(self.display, text="3", command=self.add_three).pack()
+        self.button_four = ttk.Button(self.display, text="4", command=self.add_four).pack()
+        self.button_five = ttk.Button(self.display, text="5", command=self.add_five).pack()
+        self.button_six = ttk.Button(self.display, text="6", command=self.add_six).pack()
+        self.button_seven = ttk.Button(self.display, text="7", command=self.add_seven).pack()
+        self.button_eight = ttk.Button(self.display, text="8", command=self.add_eight).pack()
+        self.button_nine = ttk.Button(self.display, text="9", command=self.add_nine).pack()
+        self.a = 0
+        self.b = "1"
+        self.c = "0"
+        self.d = "2"
+        self.e = "3"
+        self.f = "4"
+        self.g = "5"
+        self.h = "6"
+        self.i = "7"
+        self.j = "8"
+        self.k = "9"
+        self.button_zero = ttk.Button(self.display, text= "0", command= self.add_zero)
+        self.button_zero.pack()
+
+        #Functions
+        self.button_dot = ttk.Button(self.display, text= ".", command= self.convert_to_float).pack()
+        self.button_add = ttk.Button(self.display, text= "+", command= self.add)
+        self.button_add.pack()
+        self.button_delete = ttk.Button(self.display, text="Delete all", command=self.delete_all).pack()
+        self.result = ttk.Button(self.display, text = "=")
+        self.result.config(command= self.result_func)
+        self.result.pack()
+
+        self.display.mainloop()
+
+    def add_zero(self):
+        if self.a == 0:
+           self.a = str(self.a)
+        else:
+             self.a += self.c
+
+    def add_one(self):
+        if self.a == 0:
+            self.b = int(self.b)
+            self.a += self.b
+            self.b = str(self.b)
+            self.a = str(self.a)
+        else:
+            self.a += self.b
+        self.screen.config(text=self.a)
+
+    def add_two(self):
+        if self.a == 0:
+            self.d = int(self.d)
+            self.a += self.d
+            self.d = str(self.d)
+            self.a = str(self.a)
+        else:
+            self.a += self.d
+        self.screen.config(text=self.a)
+
+    def add_three(self):
+        if self.a == 0:
+            self.e = int(self.e)
+            self.a += self.e
+            self.e = str(self.e)
+            self.a = str(self.a)
+        else:
+            self.a += self.e
+        self.screen.config(text=self.a)
+
+    def add_four(self):
+        if self.a == 0:
+            self.f = int(self.f)
+            self.a += self.f
+            self.f = str(self.f)
+            self.a = str(self.a)
+        else:
+            self.a += self.f
+        self.screen.config(text=self.a)
+
+    def add_five(self):
+        if self.a == 0:
+            self.g = int(self.g)
+            self.a += self.g
+            self.g = str(self.g)
+            self.a = str(self.a)
+        else:
+            self.a += self.g
+        self.screen.config(text=self.a)
+
+    def add_six(self):
+        if self.a == 0:
+            self.h = int(self.h)
+            self.a += self.h
+            self.h = str(self.h)
+            self.a = str(self.a)
+        else:
+            self.a += self.h
+        self.screen.config(text=self.a)
+
+    def add_seven(self):
+        if self.a == 0:
+            self.i = int(self.i)
+            self.a += self.i
+            self.i = str(self.i)
+            self.a = str(self.a)
+        else:
+            self.a += self.i
+        self.screen.config(text=self.a)
+
+    def add_eight(self):
+        if self.a == 0:
+            self.j = int(self.j)
+            self.a += self.j
+            self.j = str(self.j)
+            self.a = str(self.a)
+        else:
+            self.a += self.j
+        self.screen.config(text=self.a)
+
+    def add_nine(self):
+        if self.a == 0:
+            self.k = int(self.k)
+            self.a += self.k
+            self.k = str(self.k)
+            self.a = str(self.a)
+        else:
+            self.a += self.k
+        self.screen.config(text=self.a)
+
+    def convert_to_float(self):
+        pass
+
+    def add(self):
+        self.number1 = int(self.a)
+        self.a = 0
+        self.screen.config(text= "0")
+
+    def result_func(self):
+        self.number2 = int(self.a)
+        self.a = 0
+        self.screen.config(text="0")
+        self.wynik = self.number1 + self.number2
+        print(self.wynik)
+        self.screen.config(text= self.wynik)
+
+    def delete_all(self):
+        self.wynik = 0
+        self.a = 0
+        self.screen.config(text= "0")
+
+okienko = Calculator()
